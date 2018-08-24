4a12e48f9cd1155f4a6fbd765b2f416ab974ada1
Erik Stens <finalart2005@gmail.com>
2018-08-24 02:02:17 +0200

    added start button, changed ports, for debugging purposes set countdown to 5 seconds for now

--- 48HfpCountDown.py
+++ 48HfpCountDown.py
@@ -28,9 +28,10 @@ import logging
 import os.path
 
 # Set up pins
-SD = 18     # Data - Connected to pin 18 of the first 74hc595
-SH_CP = 23  # Latch - Connected to pin 23 of all 74hc595s
-ST_CP = 24  # Latch - Connected to pin 24 of all 74hc595s
+SD = 16     # Data - Connected to pin 16 of the first 74hc595
+SH_CP = 20  # Latch - Connected to pin 20 of all 74hc595s
+ST_CP = 21  # Latch - Connected to pin 21 of all 74hc595s
+START_BUTTON = 12
 
 # Segment code from 0 to F in Hexadecimal
 #segCode = [0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71]
@@ -59,10 +60,14 @@ noSegCode = 0x80
 nShiftBits = 48
 zeroTimeout = 3600 #time in seconds after which clock will show '48 HFP' again
 
+#countdownTime = timedelta(hours = 48, minutes = 30, seconds = 0)
+countdownTime = timedelta(hours = 0, minutes = 0, seconds = 5)
+
 class State:
-    clockPaused = True
     clockFinish = datetime.now()
     currentCountdown = timedelta(seconds = -zeroTimeout)
+    clockPaused = True
+    last_start_button_state = False
 
 def print_msg():
     print('Program is running...')
@@ -82,29 +87,37 @@ def initGPIO():
     GPIO.output(SH_CP, GPIO.LOW)
     GPIO.output(ST_CP, GPIO.LOW)
 
+    GPIO.setup(START_BUTTON, GPIO.IN)
+
 def initClock(state):
-    timePath = 'clock.48h'    
+    timePath = 'clock.48h'
     
     if os.path.isfile(timePath):
         timeFile = open(timePath, 'r')
         timeStr = timeFile.read()
         timeFile.close()
 
-        state.clockPaused = False
         state.clockFinish = datetime.strptime(timeStr, "%Y/%m/%d - %H:%M:%S")
         state.currentCountdown = state.clockFinish - datetime.now()
+        state.clockPaused = False
 
         print('finish time loaded: ' + datetime.strftime(state.clockFinish, "%Y/%m/%d - %H:%M:%S"))
 
 #Shift the data to 74HC595
 def hc595_shift(dat, bits):
+    #str = ''
     leftBit = 1 << (bits - 1)
 
     GPIO.output(ST_CP, GPIO.LOW)
     for bit in range(0, bits):
         GPIO.output(SH_CP, GPIO.LOW)
-        GPIO.output(SD, (leftBit & (dat << bit)) > 0)
+        #if (leftBit & (dat << bit)) > 0:
+        #    str = str + '1'
+        #else:
+        #    str = str + '0'
+        GPIO.output(SD, GPIO.HIGH if (leftBit & (dat << bit)) > 0 else GPIO.LOW)
         GPIO.output(SH_CP, GPIO.HIGH)
+    #print str
     GPIO.output(ST_CP, GPIO.HIGH)
 
 def loop(state):
@@ -116,6 +129,33 @@ def loop(state):
                 state.currentCountdown = timedelta(seconds = -zeroTimeout)
 
             setClock(state)
+
+        if start_button_pressed(state):
+            startClock(state)
+
+
+def start_button_pressed(state):
+    button_pressed = GPIO.input(START_BUTTON)
+    #print button_pressed
+    if button_pressed and not state.last_start_button_state:
+        startClock(state)
+
+    state.last_start_button_state = button_pressed
+
+
+def startClock(state):
+    state.clockFinish = datetime.now() + countdownTime
+    state.currentCountdown = state.clockFinish - datetime.now()
+    state.clockPaused = False
+    
+    timePath = 'clock.48h'
+    timeFile = open(timePath, 'w+')
+    timeStr = datetime.strftime(state.clockFinish, "%Y/%m/%d - %H:%M:%S")
+    timeFile.write(timeStr)
+    timeFile.close()
+
+    print('started countdown to: ' + datetime.strftime(state.clockFinish, "%Y/%m/%d - %H:%M:%S"))
+
 
 def setClock(state):
     total_seconds = math.ceil(state.currentCountdown.total_seconds())
@@ -134,24 +174,29 @@ def setClock(state):
             segCode = str2segCode('000000')
         else:
             segCode = str2segCode('      ')
-        #print('000000' + " - " + '{:#018x}'.format(segCode))
+        #sprint('000000' + " - " + '{:#018x}'.format(segCode))
         hc595_shift(segCode, nShiftBits)
     else:
         segCode = str2segCode('48 HFP')
         #print('48 HFP' + " - " + '{:#018x}'.format(segCode))
         hc595_shift(segCode, nShiftBits)
 
+
 def str2segCode(str):
     segCode = 0x0000000000000000
-    for c in reversed(str):
+    #for c in reversed(str):
+    for c in str:
         segCode = (segCode << 8) | getSegCode(c)
     return segCode
+
 
 def getSegCode(c):
     return segDict.get(c, noSegCode)
 
+
 def destroy():
     GPIO.cleanup()
+
 
 if __name__ == '__main__':
     print_msg()
